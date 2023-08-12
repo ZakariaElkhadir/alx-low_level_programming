3f66cb3bbbbdc5a15ca98050d87c59c5c6409508
ZakariaElkhadir <zelkhadi5@gmail.com>
2023-08-12 18:16:05 +0100

    variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+int main() {
+    char ch;
+
+    // Print lowercase alphabet
+    for (ch = 'a'; ch <= 'z'; ch++) {
+        putchar(ch);
+    }
+
+    // Print uppercase alphabet
+    for (ch = 'A'; ch <= 'Z'; ch++) {
+        putchar(ch);
+    }
+
+    // Print a new line
+    putchar('\n');
+
+    return 0;
+}
+
