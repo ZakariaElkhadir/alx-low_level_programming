c691650f86f140a4f9e306c94292ff3ea14ac577
ZakariaElkhadir <zelkhadi5@gmail.com>
2023-08-14 00:39:21 +0100

    variables

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void){
-    //abcdefghijklmnopqrstuvwxyz
     char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
     int i;
     for (i = 25; i >= 0; i--){
