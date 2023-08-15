986a19aa842a2b29c0013c02479767c24925a43c
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-08-15 16:54:14 +0100

    hello

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - print all alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+        char ch;
+	int i;
+
+	i=0
+
+	while(i<10)
+	{
+	   ch = 'a'
+           while(ch <= 'z'){
+	   _putchar(ch)
+	   ch++;	   
+	   }
+	   _putchar('\n')
+	   i++   
+	}
+
+		
+}
