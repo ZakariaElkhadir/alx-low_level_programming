9a61af96f2767c2433e5022526657ac373d0c38d
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-08-17 00:12:24 +0100

    add

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar('+');
+printf("+");
 return (1);
 }
-else if (n == 0)
+if (n == 0)
 {
-putchar('0');  /* You should use '0' instead of 48 for better clarity*/
+printf("0");
 return (0);
 }
-else if (n < 0)
+if (n < 0)
 {
-putchar('-');
+printf("-");
 return (-1);
 }
-
+}
