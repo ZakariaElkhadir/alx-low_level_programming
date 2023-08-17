e4ea54b13f99e8fbd59c844492d8e0f631ea8d9d
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-08-17 11:15:46 +0100

    add

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,10 +7,18 @@
 
 int main(void)
 {
-        int i;
-
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
+int i;
+i = 98;
+positive_or_negative(i)
+{
+if (i > 0)
+{
+print("i is positive")
+}
+else
+{
+printf("i is negative")
+}
+}
+return (0);
 }
