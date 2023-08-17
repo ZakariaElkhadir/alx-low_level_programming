c9b9e56593e863c77e3680f8ffeb4291efff4c59
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-08-17 18:29:17 +0100

    add

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,9 @@
 #include "main.h"
   /* This loop will print the characters from 0 to 9. */
   int _isdigit(int c) {
-  return (c >= '0' && c <= '9');
+  if(c >= '0' && c <= '9'){
+		return(1);
+	else 
+		return (0);
+}
 }
