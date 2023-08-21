97d7bb071e6104f3cf625c8f3101a5c7b63ed64e
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-08-21 21:15:20 +0100

    add

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,5 @@
-#include <stdio.h>
+#include "main.h"
 
 void reset_to_98(int *n) {
   *n = 98;
-}
-
-int main() {
-  int n = 100;
-
-  reset_to_98(&n);
-
-  printf("The value of n is now %d\n", n);
-
-  return 0;
 }
