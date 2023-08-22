a33637a6db2f53ea35f34a670b4bceb451efc90f
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-08-22 23:47:02 +0100

    add

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 int _atoi(char *s)
 {
-int nb;
-
-nb = _atoi("98");
-printf("%d\n", nb);
-nb = _atoi("-402");
-printf("%d\n", nb);
-nb = _atoi("          ------++++++-----+++++--98");
-printf("%d\n", nb);
-nb = _atoi("214748364");
-printf("%d\n", nb);
-nb = _atoi("0");
-printf("%d\n", nb);
-nb = _atoi("Suite 402");
-printf("%d\n", nb);
-nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-printf("%d\n", nb);
-nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-printf("%d\n", nb);
+s = _atoi("98");
+printf("%d\n", s);
+s = _atoi("-402");
+printf("%d\n", s);
+s = _atoi("          ------++++++-----+++++--98");
+printf("%d\n", s);
+s = _atoi("214748364");
+printf("%d\n", s);
+s = _atoi("0");
+printf("%d\n", s);
+s = _atoi("Suite 402");
+printf("%d\n", s);
+s = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
+printf("%d\n", s);
+s = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+printf("%d\n", s);
 return (0);
 }
