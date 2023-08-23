86ab0b4b13f58ff412432344e93b39fd68ec5aab
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-08-23 14:30:11 +0100

    add all

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
-#include <string.h>
+char *_strcat(char *dest, char *src)
+{
+int i;
+int b;
 
-char *_strcat(char *dest, char *src) {
-  int dest_len = strlen(dest);
-  int src_len = strlen(src);
-
-  if (dest_len + src_len > 100) {
-    printf("The destination string is not large enough.\n");
-    return NULL;
-  }
-
-  strcat(dest, src);
-  return dest;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
 }
-
-int main() {
-  char dest[100];
-  char src[] = "Hello, world!";
-
-  _strcat(dest, src);
-  printf("%s\n", dest);
-
-  return 0;
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++
+}
+dest[i] = '\0';
+return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+Pointers,array  and strings are all important data structures in the c programming language 
+_pointers: are variables that store the address og another variable. they can be used to indirectly access the value of the variiable that they point to.
+
+_Arrays are variables that store the multiple values of the dame data type. The values in an array are terminated by  a null character ('\O').
+ 
