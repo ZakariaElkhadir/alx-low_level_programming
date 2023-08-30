ef5f93abbaa81cd197d42eddefeb63765fff9120
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-08-30 20:30:23 +0100

    add

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -17,11 +17,10 @@ return (0);
 return (prime2(a, b + 1));
 }
 /**
-* is_prime_number - checks if a number is prime
-* @n: Number Integer
-*
-* Return: On success 1. 
-* On error, -1 is returned, and errno is set appropriately.
+*is_prime_number - checks if a number is prime
+*@n: Number Integer
+*Return: On success 1. 
+*On error, -1 is returned, and errno is set appropriately.
 */
 int is_prime_number(int n)
 {
