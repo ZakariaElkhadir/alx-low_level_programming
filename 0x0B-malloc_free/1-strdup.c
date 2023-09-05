cf4e4e999d24323ce2b9bbdc2acc231536fc85b2
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-09-05 13:36:21 +0100

    add

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,9 +3,9 @@
 char *_strdup(char *str){
 
 	str = (char *)malloc(100 * sizeof(char));
-	_strdup("Holberton School!");
-	_strdup("First, solve the problem. Then, write the code.");  
-	_strdup(NULL);
+	if (str == NULL)
+		return NULL;
+
 	return (str);
 	free (str);
 }
