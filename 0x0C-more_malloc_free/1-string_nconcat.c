56de046b490b349f37c49ad982aeba202dfcd1b9
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-09-08 11:07:14 +0100

    add

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+char *string_nconcat(char *s1, char *s2, unsigned int n){
+	char *ptr = s1;
+	char s2;
+	int n;
+	s1 = malloc(s2);
+	s2 = str("hello world ", 6);
+	if (ptr == NULL){
+		return NULL;
+		free (ptr);
+	}
+	if (n => s2){
+		printf("%s", s2);
+	}
+	else {
+		return NULL;
+	}
+}
