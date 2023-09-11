462bef7ddf85241213364c52eb4e896a716b7ddd
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-09-11 21:33:05 +0100

    edit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+* struct fog - a dog struct 
+* @name: dog name 
+* @age: dog age
+* @owner: owner name 
+*Description: jusst a lone dog struct in a big kitty world
+*/
 struct dog
 {
 char *name;
