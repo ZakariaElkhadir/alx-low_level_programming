28bddee327ec47e5f67eb51092c6a40ab9893fe2
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-09-13 16:04:26 +0100

    add

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,8 +6,9 @@
  *
  * Return: Nothing.
  */
-void print_name(char *name, void (*f)(char *)){
- if(name == NULL || f == NULL)
-    return;
-  f(name);
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }
