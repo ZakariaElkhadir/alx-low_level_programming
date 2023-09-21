b7133bd4be04499157a523abbcea966ce62439b7
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-09-21 23:46:10 +0100

    jarvis

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,13 +16,11 @@ size_t print_list(const list_t *h)
 		if (!h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-		}
 		h = h->next;
 		zakaria++;
 
 	}
 
-	return (zakari);
+	return (zakaria);
 }
