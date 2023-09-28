c797fba2132979a0ed374a875c996b1c0ca8b885
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-09-28 19:50:39 +0100

    edit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int jv = 0;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
@@ -18,9 +18,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec_val = 2 * jv + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (jv);
+	return (dec_val);
 }
-
