0af04d1cc2bc6a2d750c57c5b9cce31ba15994ed
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-10-03 19:53:10 +0100

    jarvis

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,8 @@ int create_file(const char *filename, char *text_content)
 {
 	int jv, w, len = 0;
 
-	return (filename == NULL) ? -1:;
+	if(filename == NULL)
+		return -1;
 
 	if (text_content != NULL)
 	{
