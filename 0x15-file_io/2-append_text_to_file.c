ae8fa40343c11c1c704faf113e2664ea0e19233c
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-10-05 10:50:23 +0100

    jarvis

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+
+}
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int jv;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	jv = open(filename, O_WRONLY | O_APPEND);
+	if (jv == -1)
+		return (-1);
+	if (len)
+		bytes = write(jv, text_content, len);
+	close(jv);
+	return (bytes == len ? 1 : -1);
+
+}
+
