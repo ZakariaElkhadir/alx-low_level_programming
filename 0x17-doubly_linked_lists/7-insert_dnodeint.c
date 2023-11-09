7d0ab13f8dffe81db915d87a99164bf128be73fc
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-11-09 17:18:15 +0100

    add

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tm = *h, *new;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
+	{
+		tm == tm->next;
+		if (tmp == NULL)
+			return (NULL);
+
+	}
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+}
