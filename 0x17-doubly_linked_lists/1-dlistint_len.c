55f6c767fa7c7fb850a9435fd42235cbd254d21c
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-11-09 14:21:16 +0100

    add

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/**
+*dlistint_len - the function that take *h
+*dlistint_t list
+*@h: head of the list 
+*Return: return the number of element
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t jv = 0;
@@ -6,7 +12,7 @@ size_t dlistint_len(const dlistint_t *h)
 	while (h)
 	{
 		h = h->next;
-		jv++
+		jv++;
 	}
 	return (jv);
 }
