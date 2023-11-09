8f7f5c0c20342dcbb57eed5433b1c79b82e47181
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-11-09 11:41:04 +0100

    betty-style fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,12 +2,13 @@
 /**
  * print_distint - print the function
  * @h: head node address
- * 
+ *
  * Return: size of list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t x = 0;
+
 	while (h)
 	{
 		printf("%d\n", h->n);
