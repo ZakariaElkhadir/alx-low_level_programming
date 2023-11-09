23279452c6d9dfd5c5c63834dc3005833d5ae341
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-11-09 17:20:18 +0100

    edit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,10 +9,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	for (; idx != 1; idx--)
 	{
 		tm == tm->next;
-		if (tmp == NULL)
+		if (tm == NULL)
 			return (NULL);
 
 	}
-	if (tmp->next == NULL)
+	if (tm->next == NULL)
 		return (add_dnodeint_end(h, n));
 }
