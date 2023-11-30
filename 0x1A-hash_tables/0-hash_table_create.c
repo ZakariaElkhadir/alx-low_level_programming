457d6ba89f50ff70035fd38869d95bdf3464a238
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-11-30 18:08:56 +0100

    edit some code and fix betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 /**
- * hash_table_create - create a hash table
- * @size: the size of the array
- * Return: return the hash_table
+ * hash_table_create - function that creates a hash table
+ * @size: is the size of the array
+ * Return:  a pointer to the newly created hash table
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -19,12 +19,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (hash_table->array == NULL)
 	{
-		free(hash_table), hash_table = NULL;
+		free(hash_table);
 		return (NULL);
 	}
 
 	for (i = 0; i < size; i++)
 		hash_table->array[i] = NULL;
-	free(hash_table);
 	return (hash_table);
 }
