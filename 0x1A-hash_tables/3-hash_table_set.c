55eb57dd7d2565ae7e25ef3ae3ec17349fc540df
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-12-01 23:14:13 +0100

    edit file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(tmp->key, key) == 0)
 		{
-			new_value = strdup(tmp);
+			new_value = strdup(value);
 			if (new_value == NULL)
 				return (0);
 			free(tmp->value);
