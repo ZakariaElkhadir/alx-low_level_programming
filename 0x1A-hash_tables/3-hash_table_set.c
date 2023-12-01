b826c0f6cac4cd249100d44ad4c9a0d5073b8653
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-12-02 00:29:09 +0100

    edit code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,12 +1,47 @@
 #include "hash_tables.h"
+
 /*
-*hash_table_set - function that adds an element to the hash table.
-*@ht: hash table you want to add or update the key/value to
-*@key: is the key
-*@value: The value associated with the key.
-*
-*Return: 1 if it succeeded, 0 otherwise 
-*/
+ * make_hash_node - creates a new hash node
+ * @key: key for the node
+ * @value: for the node
+ *
+ * Return: the new node, or NULL on failure
+ */
+
+hash_node_t *make_hash_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key)
+		if (node->key == NULL)
+		{
+			free(node);
+			return (NULL);
+		}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
+
+
+
+/*
+ *hash_table_set - function that adds an element to the hash table.
+ *@ht: hash table you want to add or update the key/value to
+ *@key: is the key
+ *@value: The value associated with the key.
+ *
+ *Return: 1 if it succeeded, 0 otherwise
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
