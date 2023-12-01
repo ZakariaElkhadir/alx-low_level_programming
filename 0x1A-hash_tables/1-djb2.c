64bd307349ebc1c1e38113484e05b12019385398
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-12-01 19:20:16 +0100

    add file

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+/*
+ * hash_djb2 - implemantation of the djb2 alghorithm
+ * @str: string used to generete hash value
+ *
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+
+	unsigned int hash;
+	int c;
+
+	hash = 5381;
+	while((c = *str++))
+	{
+		hash = ((hash << 5) + c;
+				}
+				return(hash)
+				}
