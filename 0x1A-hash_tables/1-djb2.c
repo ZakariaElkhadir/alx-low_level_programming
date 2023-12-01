289cbaf893cbf0240446e91c5ae5ea72813b691c
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-12-01 19:28:00 +0100

    fix some code

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 /*
- * hash_djb2 - implemantation of the djb2 alghorithm
- * @str: string used to generete hash value
+ *hash_djb2 - implemantation of the djb2 alghorithm
+ *@str: string used to generete hash value
  *
- * Return: hash value
+ *Return: hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
@@ -12,9 +12,9 @@ unsigned long int hash_djb2(const unsigned char *str)
 	int c;
 
 	hash = 5381;
-	while((c = *str++))
+	while ((c = *str++))
 	{
-		hash = ((hash << 5) + c;
-				}
-				return(hash)
-				}
+		hash = ((hash << 5) + c);
+	}
+	return (hash);
+}
