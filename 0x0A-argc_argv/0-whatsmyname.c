64303381e93757b1d7970dedcd9b91eba2595bc0
Parrot Live user <user@localhost.localdomain>
2023-12-26 20:53:46 +0000

    edit file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+* main - take two arguments
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: return 0
+*/
 int main(int argc, char *argv[])
 {
 	printf("argc = %d\n", argc);
