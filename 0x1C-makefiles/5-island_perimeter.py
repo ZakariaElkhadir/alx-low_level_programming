87d95a925b6059e9c0940b33b09046b06e6994a9
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-01-25 00:40:25 +0100

    edit file

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""doc doc"""
 def island_perimeter(grid):
+    """island_perimeter"""
     length_row = len(grid)
     length_col = len(grid[0])
 
@@ -9,9 +11,10 @@ def island_perimeter(grid):
         for y in range(0, length_col):
             if grid[x][y] == 1:
                 par += 4
-                
+
                 if x != 0 and grid[x - 1][y] == 1:
                     connections += 1
                 if y != 0 and grid[x][y - 1] == 1:
                     connections += 1
     return par - (connections * 2)
+
