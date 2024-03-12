c483efc3065cb8bf6a06c39d35c40325bc749ed6
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-12 14:40:37 +0000

    Refactor linear_search function

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,24 +1,28 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of integers using the Linear search algorithm
+ * linear_search - searches for a value in an
+ * array of integers using the Linear search algorithm
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
  * @value: value to search for
- * Return: the first index where value is located, or -1 if value is not present in array or if array is NULL
-*/
-int linear_search(int *array, size_t size, int value){
- size_t n;
+ * Return: the first index where value is located,
+ * or -1 if value is not present in array or if array is NULL
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t n;
 
-    if (array == NULL)
-        return (-1);
-    
-    for (n = 0; n < size; n++){
-        printf("Value checked array[%ld] = [%d]\n", n, array[n]);
-        
-        if (array[n] == value)
-            return (n);
-        
-    }
-    return (-1);
+	if (array == NULL)
+		return (-1);
+
+	for (n = 0; n < size; n++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", n, array[n]);
+
+		if (array[n] == value)
+			return (n);
+
+	}
+	return (-1);
 }
